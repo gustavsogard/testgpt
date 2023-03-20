a601dec9b77e2c886a220e43731f714e500d0fa3
gubstav <guso22ab@student.cbs.dk>
2023-03-20 17:53:02 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function Footer() {
     return (
-        <div className="flex justify-center absolute bottom-4 w-full text-sm -ml-4">
+        <div className="flex justify-center absolute bottom-4 w-full text-sm -ml-4 md:-ml-10">
           <p className='text-center'>Lavet af en <Link href="https://www.linkedin.com/in/gustav-christian-s%C3%B8g%C3%A5rd/" target='_blank' className="underline">studerende</Link> på CBS, 2023</p>
         </div>
     )
