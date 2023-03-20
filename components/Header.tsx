1cfa75cc4f716220d3d6edc38bc3f402f354878b
gubstav <guso22ab@student.cbs.dk>
2023-03-20 17:37:01 +0100

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Router from 'next/router';
 
 export default function Header() {
     return (
-        <div className='flex justify-center mb-40 md:mt-0 mt-8'>
+        <div className='flex justify-center md:mb-40 md:mt-0 mb-20 mt-8'>
           <div>
             <button onClick={() => Router.reload()} className='font-bold text-xl'>
               <img src="/testgpt-logo.png" width="150" />
